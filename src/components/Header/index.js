d3a2aeb5038b04af54e91063b3823f37889ff528
Daniel <thedaniellopes@gmail.com>
2022-10-21 16:17:45 -0300

    feat: styled the Header of the page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,23 @@
+import logosvg from '../../svg/logo.svg'
 const Header = () => {
-    return(
-        <h1>Header.js</h1>
+
+    const descLogo = "this is the blog description";
+    return (
+        <>
+            <header className="flex-space-between">
+                <div className="logo">
+                    <img src={logosvg} alt={descLogo}></img>
+                </div>
+                <div className="search">
+                    <input type="text" className='input-search' name="search"></input>
+                </div>
+                <div className="menu" >
+                    <li><a href="none" className='nav-link'>Categories</a></li>
+                    <li><a href="none" className='nav-link'>About</a></li>
+                    <li><a href="none" className='nav-link'>Contact</a></li>
+                </div>
+            </header>
+        </>
     );
 }
 
